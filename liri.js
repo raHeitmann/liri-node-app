e866c840db153be28a6dd0430f5b79c093c7fdad
Ryan Arnett <ryanglennarnett@gmail.com>
2017-06-21 23:09:48 -0500

    works now

--- liri.js
+++ liri.js
@@ -2,6 +2,9 @@ var inquirer = require("inquirer");
 var twitter = require('twitter');
 var Spotify = require('node-spotify-api')
 var keys = require('./keys.js');
+var request = require('request');
+var song = '';
+
 
     var client = new twitter({
   consumer_key: 'WTzvwwmo1Ej0xS8ksPR1V3ubS',
@@ -23,6 +26,9 @@ function tweeting(){
 var params = {screen_name: 'john_johntesty'};
 client.get('statuses/user_timeline', params, function(error, tweets, response) {
   if (!error) {
+
+      console.log("")
+      console.log("")
       
      for (var i = 0; i<20; i++)
      {
@@ -31,6 +37,10 @@ client.get('statuses/user_timeline', params, function(error, tweets, response) {
         console.log("DATE AND TIME: '"+tweets[i].created_at);
         console.log("")
      }
+
+     console.log("")
+     console.log("")
+
   }
 });
        
@@ -40,6 +50,7 @@ client.get('statuses/user_timeline', params, function(error, tweets, response) {
 //SPOTIFY 
 
 function spotify(){
+
 
 
     inquirer.prompt([
@@ -78,13 +89,49 @@ function spotify(){
 
 function omdb(){
 
-    //TODOfire when processargv2 = movie-this
+//   * Title of the movie.
+//   * Year the movie came out.
+//   * IMDB Rating of the movie.
+//   * Country where the movie was produced.
+//   * Language of the movie.
+//   * Plot of the movie.
+//   * Actors in the movie.
+//   * Rotten Tomatoes URL.
 
-}
 
-function dowhat(){
-    
-    //TODOfire when processargv2 = do-what-it-says
+ inquirer.prompt([
+
+        {
+        type: "input",
+        message: "What is the movie you would like to search?",
+        name: "movieName"
+    }
+
+    ]).then(function(user) {
+ 
+        
+request('http://www.omdbapi.com/?apikey=40e9cece&t='+user.movieName+'&r=json', function (error, response, body) {
+    console.log("");
+    console.log("");
+    console.log("");
+  console.log('error:', error); // Print the error if one occurred 
+  console.log('statusCode:', response && response.statusCode);
+  var info = JSON.parse(body) // Print the response status code if a response was received 
+  console.log('Title:', info.Title);
+  console.log('Year:', info.Year);
+  console.log('imdbRating:', info.imdbRating);
+  console.log('Country:', info.Country);
+  console.log('Language:', info.Language);
+  console.log('Plot:', info.Plot);
+  console.log('Actors:', info.Actors);
+  console.log('Plot:', info.Website);
+  console.log("");
+  console.log("");
+  console.log("");
+});
+
+ 
+});
 
 }
 
@@ -116,74 +163,37 @@ else if (user.action === 'Spotify') {
 
 else if (user.action === 'Movie') {
 
-//TODO twitter();
-
-    console.log("==============================================");
-    console.log("");
-    console.log("Welcome " + user.name);
-    console.log("Your favorite color is blue");
-    console.log("You are interested in just about everything!");
-    
-    console.log("==============================================");
-
- 
+    omdb();
 }
 
 else if (user.action === 'Do-What-It-Says') {
 
-//TODO twitter();
+    const fs = require('fs');
+   fs.readFile('./random.txt', 'utf8', (err, data) => {
+  if (err) throw err;
 
-    console.log("==============================================");
-    console.log("");
-    console.log("Welcome " + user.name);
-    console.log("Your favorite color is blue");
-    console.log("You are interested in just about everything!");
+  data = data.split(',');
+  song = data[1];
+
+  spootifu.search({ type: 'track', query: song }, function(err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        
+        console.log("");
+        console.log("");
+        console.log("Artist(s): "+data.tracks.items[0].album.artists[0].name);
+        console.log("Album: "+data.tracks.items[0].album.name); 
+        console.log("Track: "+data.tracks.items[0].name); 
+        console.log("Preview Link (cmd+double click to listen in browser!): ");
+        console.log(data.tracks.items[0].preview_url); 
+        console.log("");
+        console.log("");
+
+    });
     
-    console.log("==============================================");
-
+   });
  
 }
 
 })
-
-//   else {
-// inquirer.prompt([
-//     {
-//     type: "list",
-//     message: "What is your favorite color?",
-//     choices: ["Green", "Blue", "Red", "Purple", "Yellow", "Orange", "Black"],
-//     name: "color"
-//   },
-
-//     {
-//     type: "checkbox",
-//     message: "What interests you?",
-//     choices: ["The Outdoors", "People", "Space", "Nothing"],
-//     name: "interest"
-//   },
-
-
-//   {
-//     type: "confirm",
-//     message: "Are you sure:",
-//     name: "confirm",
-//     default: true
-
-//   }
-// ]).then(function(user) {
-
-// console.log(JSON.stringify(user, null, 2));
-//  if (user.confirm) {
-
-    // console.log("==============================================");
-    // console.log("");
-    // console.log("Welcome " + currentUser);
-    // console.log("Your favorite color is " + user.color);
-    // console.log("You are interested in "+user.interest);
-    
-    // console.log("==============================================");
-
-  // If the user does not confirm, then a message is provided and the program quits.
-
-
-
